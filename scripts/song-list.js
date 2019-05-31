35d263a0e71be816b6fa9576f12064588df7d8a3
robschaefer <robschaefer13@gmail.com>
2019-05-31 11:00:13 -0700

    Checkpoint 10 commit: PlayerBar_Buttons

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -2,23 +2,24 @@
   album.songs.forEach( (song, index) => {
     song.element = $(`
       <tr>
-      <td>
+       <td>
         <button>
           <span class="song-number">${index + 1}</span>
           <span class="ion-play"></span>
           <span class="ion-pause"></span>
         </button>
-      </td>
-      <td>${song.title}</td>
-      <td>${song.duration}</td>
-       </tr>
+       </td>
+       <td>${song.title}</td>
+       <td>${song.duration}</td>
+      </tr>
     `);
 
         song.element.on('click', event => {
           player.playPause(song);
+          $('button#play-pause').attr('playState', player.playState);
         });
 
-    $('#song-list').append(song.element);
+        $('#song-list').append(song.element);
   });
 
 }
